0939977fbccca37ed8164a21e1005f4d32a445ba
Chen Lei(陳磊) <chin_rai@b-engineer.biz>
2025-07-01 17:16:03 +0900

    0701-2 改了我的任务页面格局

--- app/src/main/java/com/aplus/remotenursing/models/UserTask.java
+++ app/src/main/java/com/aplus/remotenursing/models/UserTask.java
@@ -3,9 +3,13 @@ package com.aplus.remotenursing.models;
 public class UserTask {
     private String task_id;
     private String task_type;
+    private String task_name;
+    private int task_order;
 
     public UserTask() {}
 
     public String getTask_id() { return task_id; }
     public String getTask_type() { return task_type; }
+    public String getTask_name() { return task_name; }
+    public int getTask_order() { return task_order; }
 }
